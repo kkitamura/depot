be2022a48e2b3dbe2da5698b18298e83d11bff16
Kotaro KITAMURA <kotaro.kitamura@gaiax.com>
2013-04-25 18:12:27 +0900

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  validates :title, :image_url, :description, presence: true
+  validates :price, numericality:{ greater_than_orequal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:   %r{\.(gif|jpg|png)$}i,
+    message: 'have to be url of image'
+  }
 end
